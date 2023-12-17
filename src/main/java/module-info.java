4f693f3b8f7c5bb04c06a1bf5a4c45cd8c3a4503
Zliced13 <07e5297d5b@c0x0.com>
2023-12-16 21:04:06 -0700

    More initial commits pt 3

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.casual.flawless.src {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.casual.flawless.src to javafx.fxml;
+    exports com.casual.flawless.src;
+}
