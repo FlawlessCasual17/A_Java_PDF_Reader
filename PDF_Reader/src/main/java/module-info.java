169212b5769c5bdfc32d40fc4bba9efd4a130167
Zliced13 <07e5297d5b@c0x0.com>
2023-12-16 23:39:14 -0700

    Added Maven stuff
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

---
+++ PDF_Reader/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.casual.flawless.pdf_reader {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.casual.flawless.pdf_reader to javafx.fxml;
+    exports com.casual.flawless.pdf_reader;
+}
