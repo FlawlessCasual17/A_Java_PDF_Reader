0e9a61d2e15332cd3ca71184d7bb16b33e9aedf7
FlawlessCasual17 <07e5297d5b@c0x0.com>
2023-12-17 20:56:18 -0700

    Added some new shit.

--- src/main/java/com/casual/flawless/java_pdf_reader/HelloApplication.java
+++ src/main/java/com/casual/flawless/java_pdf_reader/HelloApplication.java
@@ -1,21 +1,31 @@
 package com.casual.flawless.java_pdf_reader;
 
+import com.pixelduke.transit.Style;
+import com.pixelduke.transit.TransitTheme;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//import java.io.File;
 import java.io.IOException;
+//import java.nio.file.Paths;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240); stage.setTitle("Hello!"); stage.setScene(scene);
-        stage.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication
+            .class.getResource("hello-view.fxml"));
+        var scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+//        var path = String.valueOf(Paths
+//            .get("src", "main", "resources", "com",
+//                "casual", "flawless", "java_pdf_reader", "dark-theme.css"));
+//        var stylePath = new File(path);
+        var transit = new TransitTheme(Style.DARK);
+        transit.setScene(scene);
+//        scene.getStylesheets().add(stylePath.toURI().toURL().toExternalForm());
+        stage.setTitle("Java PDF Reader"); stage.setScene(scene); stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) { launch(); }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,11 @@ module com.casual.flawless.java_pdf_reader {
     requires javafx.web;
 
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires com.pixelduke.transit;
 
     opens com.casual.flawless.java_pdf_reader to javafx.fxml;
     exports com.casual.flawless.java_pdf_reader;
